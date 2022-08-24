cf3925d7e944a57cc99ca8220be6870af6347b78
LA-248 <luca123@skiff.com>
2022-08-25 00:59:01 +0200

    Change if statement

--- main.rb
+++ main.rb
@@ -1,10 +1,13 @@
 def caesar_cipher(string, shift)
   cipher_text = ""
   string.each_char do |letter|
+
     if letter.ord.between?(65, 90) || letter.ord.between?(97, 122)
       new_value = letter.ord + shift
-      
-      new_value -= 26 if(new_value.between?(91, 96)) || new_value > 122
+
+      if new_value.between?(91, 96) || new_value > 122
+        new_value -= 26
+      end
       cipher_text += new_value.chr
     else
       cipher_text += letter.chr
@@ -13,4 +16,4 @@ def caesar_cipher(string, shift)
   cipher_text
 end
 
-puts caesar_cipher("What a string!", 5) # => Bmfy f xywnsl!
+p caesar_cipher("What a string!", 5) #=> Bmfy f xywnsl!
