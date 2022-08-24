93564343e3c695dfb84814c106551be0976e5fe6
LA-248 <luca123@skiff.com>
2022-08-24 21:31:59 +0200

    Initial commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+def caesar_cipher(string, shift)
+  cipher_text = ""
+  string.each_char do |letter|
+    if letter.ord.between?(65, 90) || letter.ord.between?(97, 122)
+      new_value = letter.ord + shift
+      
+      new_value -= 26 if(new_value.between?(91, 96)) || new_value > 122
+      cipher_text += new_value.chr
+    else
+      cipher_text += letter.chr
+    end
+  end
+  cipher_text
+end
+
+puts caesar_cipher("What a string!", 5) # => Bmfy f xywnsl!
